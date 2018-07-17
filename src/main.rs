27b1b8412dd8d1d1bba3e06b09d4a01a19c4f27b
Jorge Ortiz <ecnavda@gmail.com>
2018-07-17 08:28:20 -0400

    Now properly working. Compares the tuples by the first value if the second value is equal.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     println!("Enter weights to be sorted.");
 
-    println!("{}", order_weight("103 123 4444 99 2000"));
+    println!("{}", order_weight("2000 10003 1234000 44444444 9999 11 11 22 123"));
 }
 
 fn order_weight(s: &str) -> String {
@@ -9,14 +9,19 @@ fn order_weight(s: &str) -> String {
     let mut modified = Vec::<(String, i32)>::new();
     let mut weight: i32;
     for y in original {
-        weight = 0;
         weight = y.chars()
             .map( |a| a.to_digit(10).unwrap() as i32)
             .sum();
         modified.push((y.to_string(), weight));
     }
 
-    modified.sort_by_key(|a| a.1);
+    modified.sort_by(|a, b| {
+        if a.1 == b.1 {
+            return a.0.cmp(&b.0);
+        } else {
+            return a.1.cmp(&b.1);
+        };
+    });
 
     let output: Vec<String> = modified.into_iter().map( |a| a.0).collect();
     output.join(" ")
